7d08e206f74930b51ce253595e8513c2d3cf098e
nitishranjan27 <nk47595@gmail.com>
2022-02-02 18:00:56 +0530

    Nitish Added UC1 Program

---
+++ MoodAnayzerProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MoodAnayzerProblem;
+
+namespace MoodAnalyzerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // UC1
+            MoodAnalyse obj = new MoodAnalyse("Sad");
+            obj.AnalyseMood();
+        }
+    }
+}
