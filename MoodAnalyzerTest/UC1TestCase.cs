c04dfad35d6f4c8132fe7568974d4b9ffa42b7f0
nitishranjan27 <65806427+nitishranjan27@users.noreply.github.com>
2022-02-03 18:23:43 +0530

    Update UC1TestCase.cs

--- MoodAnalyzerTest/UC1TestCase.cs
+++ MoodAnalyzerTest/UC1TestCase.cs
@@ -1,21 +1,42 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MoodAnayzerProblem;
 
-namespace MoodAnalyzerMSTestWithCore
+namespace MoodAnalyzerProblem
 {
     [TestClass]
-    public class UnitTest2
+    public class UnitTest1
     {
+        /// <summary>
+        /// TC 1.1: Given "I am  in Sad Mood" message Should Return SAD.
+        /// </summary>
         [TestMethod]
-        [DataRow(null)]
-        public void GivenHAPPYMoodShouldReturnHappy(string message)
+        public void GivenSadMoodShouldReturnSAD()
+        {
+            // Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            MoodAnalyse moodAnalyse = new MoodAnalyse(message);
+
+            // Act
+            string mood = moodAnalyse.AnalyseMood();
+
+            // Assert
+            Assert.AreEqual(expected, mood);
+        }
+
+        /// <summary>
+        /// TC 1.2: Given "I am  in Any Mood" message Should Return HAPPY.
+        /// </summary>
+        [TestMethod]
+        public void GivenAnyMoodShouldReturnHAPPY()
         {
             // Arrange
             string expected = "HAPPY";
-            MoodAnalyse2 moodAnalyzer = new MoodAnalyse2(message);
+            string message = "I am in Any Mood";
+            MoodAnalyse moodAnalyse = new MoodAnalyse(message);
 
-            //Act
-            string mood = moodAnalyzer.AnalyseMood();
+            // Act
+            string mood = moodAnalyse.AnalyseMood();
 
             // Assert
             Assert.AreEqual(expected, mood);
